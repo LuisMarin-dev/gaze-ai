483de3fa959cf2caa68bb084d06eb812d3f80786
Luis Marin <luis.marinprog@gmail.com>
2022-09-18 00:53:31 -0400

    Changed from function to class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React, {Component} from "react";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
 // import ParticlesBackground from './components/ParticlesBackground';
 
-function App() {
+class App extends Component {
   constructor(){
     super();
     this.state = {
@@ -13,22 +14,56 @@ function App() {
     }
   }
 
-  onInputChange = (envent) => {
-    console.log(event);
+  onInputChange = (event) => {
+    console.log(event.target.value);
   }
 
-  return (
-    <div className="App">
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} />
-         {/*
-        <FaceRecognition /> 
-        <ParticlesBackground/>
-        */}
-    </div>
-  );
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render(){
+    return (
+      <div className="App">
+          <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+          {/*
+          <FaceRecognition /> 
+          <ParticlesBackground/>
+          */}
+      </div>
+    );
+  }
 }
+
+
+
+// function App() {
+//   constructor(){
+//     super();
+//     this.state = {
+//       input: '',
+//     }
+//   }
+
+//   onInputChange = (event) => {
+//     console.log(event);
+//   }
+
+//   return (
+//     <div className="App">
+//         <Navigation />
+//         <Logo />
+//         <Rank />
+//         <ImageLinkForm onInputChange={this.onInputChange} />
+//          {/*
+//         <FaceRecognition /> 
+//         <ParticlesBackground/>
+//         */}
+//     </div>
+//   );
+// }
 
 export default App;
